9f325dfbb4cfb1c0ca62d7e9ed667edc2bdaffa7
rasenin <romansenin15@gmail.com>
2019-05-22 21:11:21 -0700

    wrote postItem function

--- great-bay.js
+++ great-bay.js
@@ -17,3 +17,43 @@ var connection = mysql.createConnection({
 });
 
 //Table-name: items
+
+function postItem(itemName, bidderName, priceName) {
+  console.log("Inserting a new playlist...\n");
+  var query = connection.query(
+    "INSERT INTO items SET ?",
+    {
+      item: itemName,
+      bidder: bidderName,
+      price: priceName
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " item inserted!\n");
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function bidItem() {
+
+  var query = connection.query("SELECT * FROM items", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+  });
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function readItems() {
+  console.log("Selecting all items...\n");
+  connection.query("SELECT * FROM items", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+  });
+}
